402138a3e97661adf6be4931dc0387c592d00d6e
NikolaiMiroshnichenko <Nikolai.Miroshnichenko26@gmail.com>
2021-02-17 18:28:25 +0300

    Add project files.

---
+++ App1/Fragments/Fragment2.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace App1.Fragments
+{
+    public class Fragment2 : Android.Support.V4.App.Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            HasOptionsMenu = true;
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = inflater.Inflate(Resource.Layout.fragment2_layout, container, false);
+            return view;
+        }
+
+        public override void OnResume()
+        {
+            if (Activity is MainActivity activity)
+            {
+                activity.SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            }
+            base.OnResume();
+
+        }
+
+    }
+}
---
+++ App1/MainActivity.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.Design.Widget;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+using App1.Fragments;
+
+namespace App1
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private LinearLayout _linearLayout;
+        private Android.Support.V7.Widget.Toolbar _toolbar;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            _toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(_toolbar);
+
+
+            _linearLayout = FindViewById<LinearLayout>(Resource.Id.rootLayout);
+
+            var transaction = SupportFragmentManager.BeginTransaction();
+            transaction.Replace(Resource.Id.rootLayout, new StartFragment());
+            transaction.Commit();
+            _toolbar.Click += toolbarClick;
+        }
+
+        private void toolbarClick(object sender, EventArgs e)
+        {
+            OnBackPressed();
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        //public override bool OnOptionsItemSelected(IMenuItem item)
+        //{
+        //    int id = item.ItemId;
+        //    if (id == 16908332)
+        //    {
+        //        //var transaction = SupportFragmentManager.BeginTransaction();
+        //        //transaction.Replace(Resource.Id.rootLayout, new StartFragment());
+        //        //transaction.Commit();
+        //        //return true;
+        //        OnBackPressed();
+        //    }
+  
+        //    return true;
+        //}
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
